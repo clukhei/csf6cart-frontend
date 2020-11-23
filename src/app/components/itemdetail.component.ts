9140658f2363d4c43d50dc83d3c1548707d1c0c8
Lu Khei <kheiboardwarrior@gmail.com>
2020-11-23 21:48:48 +0800

    reset button must be outside of form for validatioin error to not appear on reset

--- src/app/components/itemdetail.component.ts
+++ src/app/components/itemdetail.component.ts
@@ -17,13 +17,14 @@ export class ItemdetailComponent implements OnInit, OnChanges {
 
   constructor(private fb: FormBuilder) { }
 
+
   ngOnInit(): void {
     /*   this.form= this.fb.group({
         item: this.fb.control(""),
         quantity: this.fb.control("")
    
       }) */
-
+    console.log(this.form)
   }
 
   createForm(item: CartItem = null): FormGroup {
@@ -31,14 +32,17 @@ export class ItemdetailComponent implements OnInit, OnChanges {
       id: this.fb.control(item?.id, [Validators.required]), // ? is for --> if item is null, leave blank
       item: this.fb.control(item?.item, [Validators.required, Validators.minLength(3)]),
       quantity: this.fb.control(item?.quantity, [Validators.required, Validators.min(1), Validators.max(100)])
-
+      
     })
+
   }
 
 
   ngOnChanges() {
     //creates a new instance of the form every time the data changes
     this.form = this.createForm(this.data)
+
+
     /*     this.form.get('item').setValue(this.data.item)
         this.form.get('quantity').setValue(this.data.quantity) */
   }
@@ -53,21 +57,20 @@ export class ItemdetailComponent implements OnInit, OnChanges {
        this.form.get('quantity').patchValue(this.form.get('quantity').value)
        console.log('updated')
        console.log(this.form.value) */
-       this.form.reset()
+    this.form.reset()
   }
 
-  
-  reset(formDirective) {
-    formDirective.resetForm()
 
+  reset() {
+    this.form.reset()
   }
 
-  deleteItem(){
+  deleteItem() {
     const id = this.form.get('id').value
     this.onDelete.emit(id)
     this.form.reset()
   }
-  addItem(){
+  addItem() {
     const item = this.form.value
     this.onAdd.emit(item)
     this.form.reset()
